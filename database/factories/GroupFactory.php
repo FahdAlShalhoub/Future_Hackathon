dd0a7681b1cbe8f82876820b019bd4a1ff0f3eb3
Fahd <FahdShalhoub@gmail.com>
2019-08-23 16:57:33 +0300

    edited the seeders to fit the new database schemas

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -3,13 +3,15 @@
 use Faker\Generator as Faker;
 use App\Group;
 $factory->define(Group::class, function (Faker $faker) {
+    $groupMembersJson=[];
+    for($i=1;$i<=$faker->numberBetween($min = 1 , $max = 3);$i++){
+        $groupMembersJson+=[$i+1 => $faker->firstname.' '.$faker->lastname];
+    }
     return [
         'leadName'=> $faker->firstname.' '.$faker->lastname,
         'leadEmail'=> $faker->email,
-        'leadUniID'=> $faker->randomNumber($nbDigits=5,$strict=false),
         'groupName'=>$faker->word(),
-        'track'=>$faker->randomElement($array=array('صحي','تقني','هندسي','تمويلي','بيئي')),
         'status'=>$faker->randomElement($array=array('accepted','new','rejected')),
-        'groupMembersJson'=> '{ "'.$faker->firstname.'" :'.'"'.$faker->randomNumber($nbDigits=5,$strict=false).'"'.',"'.$faker->firstname.'" :'.'"'.$faker->randomNumber($nbDigits=5,$strict=false).'"'.'}',
+        'groupMembersJson'=> json_encode($groupMembersJson),
     ];
 });
--- database/factories/SingleFactory.php
+++ database/factories/SingleFactory.php
@@ -7,11 +7,7 @@ $factory->define(Single::class, function (Faker $faker) {
         'email'=>$faker->email,
         'name'=>$faker->firstname.' '.$faker->lastname,
         'gender'=>$faker->randomElement($array = array ('female','male',)),
-        'traits'=>$faker->randomElement($array = array ('مبدع','مختلف','عبقري','مصمم','مبرمج','طموح','مفكر','باحث','خبير')).' '.$faker->randomElement($array = array ('مبدع','مختلف','عبقري','مصمم','مبرمج','طموح','مفكر','باحث','خبير')),
-        'track'=>$faker->randomElement($array=array('صحي','تقني','هندسي','تمويلي','بيئي')),
-        'skills'=>$faker->sentence($nbWords = 6, $variableNbWords = true),
         'major'=>$faker->word(),
         'status'=>$faker->randomElement($array=array('accepted','new','rejected')),
-        'uniID'=>$faker->randomNumber($nbDigits=5,$strict=false),
     ];
 });
