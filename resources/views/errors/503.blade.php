c17bc2d77ddef1291974ac9cb60271ea77a21509
Fahd <FahdShalhoub@gmail.com>
2019-09-09 13:28:36 +0300

    made maintenance page and teaser

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ar">
+    <head>
+        <title>إنتظرونا</title>
+        @include('layouts.masterHead')
+        <style>
+            body, html {
+            height: 100%;
+            }
+
+            .bg {
+            background-image: url("../img/FHteaser.png");
+            height: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="bg">
+        </div>
+    </body>
+</html>
+
+
+  
