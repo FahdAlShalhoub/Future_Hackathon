dd4b495709d54d93f88a3dcc0e6f0e5d2c8958d5
Fahd <FahdShalhoub@gmail.com>
2019-09-14 17:06:49 +0300

    wrote a basic confirmation message

--- resources/views/email/confiramtion.blade.php
+++ resources/views/email/confiramtion.blade.php
@@ -1,3 +1,26 @@
-<div>
-    Hello there test email
-</div>
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+    @include('layouts.masterHead')
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center" >
+                <img style="max-width:100%; max-height:600px" src="/img/FHlogo.png">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h1>تم قبولكم لدخول هاكاثون المستقبل</h1>
+                <h3> نرجو تأكيد حضوركم بالضغط على <a href="#"><u>الرابط</u></a></h3>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+
+</html>
