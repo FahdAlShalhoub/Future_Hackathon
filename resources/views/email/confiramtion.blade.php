8f2b9b8aebf6251cf2eb07ca073fde6aa8d02ac6
Fahd <FahdShalhoub@gmail.com>
2019-09-30 04:35:05 +0300

    formatted the confirmation email to display the name of the contestant

--- resources/views/email/confiramtion.blade.php
+++ resources/views/email/confiramtion.blade.php
@@ -2,21 +2,18 @@
 <html lang="ar">
 
 <head>
-    @include('layouts.masterHead')
+   
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center" >
-                <img style="max-width:100%; max-height:600px" src="/img/FHlogo.png">
-            </div>
+    <div>
+        <div style="text-align: center;">
+            <img style="max-width:100%; max-height:400px" src="http://www.futurehackathon.net/img/FHnoBg.png">
         </div>
-        <div class="row">
-            <div class="col-12">
-                <h1>تم قبولكم لدخول هاكاثون المستقبل</h1>
-                <h3> نرجو تأكيد حضوركم بالضغط على <a href="#"><u>الرابط</u></a></h3>
-            </div>
+        <div style="text-align: center;">
+            <h1> {{$name}} السلام عليكم</h1>
+            <h1>يسعدنا ابلاغكم بأنه تم قبولكم لدخول هاكاثون المستقبل</h1>
+            <h3> نرجو تأكيد حضوركم بالضغط على <a href="{{$url}}"><u>الرابط</u></a></h3>
         </div>
     </div>
 </body>
