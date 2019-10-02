7c194c554a15868f886a4e37247c6f747a255fc8
Fahd <FahdShalhoub@gmail.com>
2019-10-02 03:48:36 +0300

    finshed the confirmed page

--- resources/views/email/confirmed.blade.php
+++ resources/views/email/confirmed.blade.php
@@ -6,13 +6,22 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="row justifiy-content-center">
-            <div class="jumbotron">
-                <h1> تم تأكيد تسجيلك بنجاح</h1>
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner text-center">  
+                <img class="img-responsive" src="img/FHnoBgWhite.png">
+                <hr>
+                <div class="container-fluid">
+                    <div class="row">
+                        <h1 class="col-lg-12 text-center" >تم تأكيد حضوركم</h1>
+                    </div>
+                    <div class="row">
+                        <h2 class="col-lg-12 text-center" >نراكم في <a style="color:dodgerblue; text-decoration:underline;" href="https://g.page/6722space?share">Coworking Space 67|22</a></h2>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>        
+    </header>     
 </body>
 
 </html>
