0d97bf89090f4371aa975105992b09022e54cd2c
Fahd <FahdShalhoub@gmail.com>
2019-10-02 03:04:59 +0300

    added a type variable and a changed $name to $info

--- app/Mail/Confirmation.php
+++ app/Mail/Confirmation.php
@@ -11,19 +11,21 @@ class Confirmation extends Mailable
 {
     use Queueable, SerializesModels;
 
-    protected $name;
+    protected $info;
     protected $url;
+    protected $type;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($name,$url)
+    public function __construct($info,$url,$type)
     {
         
-        $this->name=$name;
+        $this->info=$info;
         $this->url=$url;
+        $this->type=$type;
 
     }
 
@@ -34,6 +36,6 @@ class Confirmation extends Mailable
      */
     public function build()
     {
-        return $this->view('email.confiramtion')->with(['name' => $this->name, 'url' => $this->url]);
+        return $this->view('email.confiramtion')->with(['info' => $this->info, 'url' => $this->url,'type' => $this->type]);
     }
 }
